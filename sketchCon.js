8223c180c46efeab65bbc1f1f6d64778e4182957
Jo Kwon <jo.kwon@duke.edu>
2018-04-17 10:40:44 -0400

    add rain

--- sketchCon.js
+++ sketchCon.js
@@ -4,6 +4,10 @@ var xoff = 0.0;
 var mySound;
 var table;
 var button;
+var drops = [];
+
+//total number of drops we want to use
+var totalDrops = 0;
 
 //function preload: image, font, sound, words
 function preload() {
@@ -19,8 +23,7 @@ function setup(){
 
 function draw(){
     background(0);
-    image(img, 0, 10, img.width/2, img.height/2);
-    //words of the lyrics in the background song
+    image(img, 0, 10, img.width/3, img.height/3);
     textAlign(CENTER);
     textFont(fontItalic);
     strokeWeight(1);
@@ -31,8 +34,45 @@ function draw(){
     createA('https://aahvs.duke.edu/people/profile/jo-kwon', 'this is a link');
     var t = 'jo.kwon@duke.edu\n https://aahvs.duke.edu/people/profile/jo-kwon'
     text(t, windowWidth/2, 200);
-    noLoop();
 
+    // Initialize one drop
+    drops[totalDrops] = new Drop();
+    // Increment totalDrops
+    totalDrops++ ;
+    // If we hit the end of the array
+    if (totalDrops >= 500) {
+        totalDrops = 0; //Start over
+    }
+      // Move and display drops
+  for (var i = 0; i < totalDrops; i++ ) { 
+    drops[i].move();
+    drops[i].display();
+    drops[i].wind();
+  }
 }
 
-
+function Drop() {
+    this.r = 50;                 
+    this.x = random(width);     
+    this.y = -this.r*4;              
+    this.speed = random(1, 7);   // Pick a random speed
+    this.wind_speed = 0.3;
+    this.c = color(50, 100, 150); // Color
+  
+    // Move the raindrop down
+    this.move = function() {
+      this.y += this.speed;
+    }
+  
+    this.wind = function(){
+      this.x += this.wind_speed;
+    }
+  
+    // Display the raindrop
+    this.display = function() {
+      // Display the drop
+      fill(this.c);
+      noStroke();
+      image(img, this.x, this.y, this.r*0.5, this.r*0.5);
+    }
+  }
